5e31a074cc9a3f97f650b5a54b3e424c52cbbd7c
varun <varunkumararya@virtualemployee.com>
2025-06-19 21:14:06 +0530

    ngrok

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://f3bc-2405-201-801d-8863-c591-97a8-abb9-2c8d.ngrok-free.app";
+export const BASE_URL = "https://aa76-2405-201-801d-8863-1ce0-2554-1966-b29d.ngrok-free.app";
